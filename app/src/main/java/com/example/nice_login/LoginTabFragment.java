46f1c177eb4afc580820d66753a7460ff86c7b97
Abdullah Kul <abdullahkul1998@gmail.com>
2023-08-12 09:59:54 +0300

    orign

---
+++ app/src/main/java/com/example/nice_login/LoginTabFragment.java
@@ -0,0 +1,24 @@
+package com.example.nice_login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginTabFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginTabFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_tab, container, false);
+    }
+}
